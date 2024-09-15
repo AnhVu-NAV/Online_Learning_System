618f5b8aaba49151de4589215fbaa717ecdfa58f
VuLH <vulhhe130330@fpt.edu.vn>
2024-09-15 10:00:55 +0700

    Add model Account, UserLog, Setting, SettingType and their respectively DAO

---
+++ src/java/model/Account.java
@@ -0,0 +1,126 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author 84941
+ */
+public class Account {
+
+    private int id;
+    private String email;
+    private String first_name;
+    private String last_name;
+    private String password;
+    private Date dob;
+    private boolean gender;
+    private int role_id;
+    private Date created_date;
+    private int status;
+
+    public Account() {
+    }
+
+    public Account(int id, String email, String first_name, String last_name, String password, Date dob, boolean gender, int role_id, Date created_date, int status) {
+        this.id = id;
+        this.email = email;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.password = password;
+        this.dob = dob;
+        this.gender = gender;
+        this.role_id = role_id;
+        this.created_date = created_date;
+        this.status = status;
+    }
+
+    
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public int getRole() {
+        return role_id;
+    }
+
+    public void setRole(int role_id) {
+        this.role_id = role_id;
+    }
+
+    public Date getCreated_date() {
+        return created_date;
+    }
+
+    public void setCreated_date(Date created_date) {
+        this.created_date = created_date;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    
+
+}
