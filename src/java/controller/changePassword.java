ec05ee671049675caa014193dc653655ddb8c043
TuanNA <tuannahe182193@fpt.edu.vn>
2024-09-19 11:26:54 +0700

    Update ChangePassword

--- src/java/controller/changePassword.java
+++ src/java/controller/changePassword.java
@@ -4,41 +4,26 @@
  */
 package controller;
 
-<<<<<<<< HEAD:src/java/controller/UserProfileController.java
-import dal.UserDAO;
-========
 import dal.CustomerDAO;
->>>>>>>> 87dc0d2 (Added ChangePass and ResetPass function):src/java/controller/changePassword.java
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-<<<<<<<< HEAD:src/java/controller/UserProfileController.java
-import model.User;
-========
 import jakarta.servlet.http.HttpSession;
 import model.Customer;
->>>>>>>> 87dc0d2 (Added ChangePass and ResetPass function):src/java/controller/changePassword.java
 
 /**
  *
  * @author admin
  */
-<<<<<<<< HEAD:src/java/controller/UserProfileController.java
-public class UserProfileController extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-========
-public class changePassword extends HttpServlet {
+public class ChangePassword extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
      *
->>>>>>>> 87dc0d2 (Added ChangePass and ResetPass function):src/java/controller/changePassword.java
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -52,17 +37,10 @@ public class changePassword extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-<<<<<<<< HEAD:src/java/controller/UserProfileController.java
-            out.println("<title>Servlet UserProfileController</title>");  
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet UserProfileController at " + request.getContextPath () + "</h1>");
-========
             out.println("<title>Servlet changePass</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet changePass at " + request.getContextPath() + "</h1>");
->>>>>>>> 87dc0d2 (Added ChangePass and ResetPass function):src/java/controller/changePassword.java
             out.println("</body>");
             out.println("</html>");
         }
@@ -79,35 +57,9 @@ public class changePassword extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-<<<<<<<< HEAD:src/java/controller/UserProfileController.java
-    throws ServletException, IOException {
-        // Lấy thông tin người dùng từ request hoặc session
-        int userId = Integer.parseInt(request.getParameter("userId"));
-
-        // Khởi tạo đối tượng UserDAO
-        UserDAO accountDAO = new UserDAO();
-        
-        // Lấy thông tin chi tiết của người dùng từ cơ sở dữ liệu
-        User user = accountDAO.getUserById(userId);
-        
-        // Kiểm tra nếu người dùng không tồn tại
-        if (user == null) {
-            // Xử lý lỗi hoặc chuyển hướng về trang lỗi
-            response.sendRedirect("error.jsp");
-            return;
-        }
-        
-        // Đặt thông tin người dùng vào request
-        request.setAttribute("user", user);
-        
-        // Chuyển tiếp đến trang JSP hiển thị thông tin người dùng
-        request.getRequestDispatcher("UserProfile.jsp").forward(request, response);
-    } 
-========
             throws ServletException, IOException {
         processRequest(request, response);
     }
->>>>>>>> 87dc0d2 (Added ChangePass and ResetPass function):src/java/controller/changePassword.java
 
     /**
      * Handles the HTTP <code>POST</code> method.
@@ -131,14 +83,14 @@ public class changePassword extends HttpServlet {
             if (new_pass.equals(c_new_pass)){
                 CustomerDAO cus = new CustomerDAO();
                 cus.updatePass(nuser.getEmail(), new_pass);
-                response.sendRedirect("AccountController");
+                response.sendRedirect("ChangePassword.jsp");
             } else {
                 request.setAttribute("error", "You entered confirm password diffent new password!");
                 request.getRequestDispatcher("changePass.jsp").forward(request, response);
             }
         } else {
             request.setAttribute("error", "Password is not correct!");
-            request.getRequestDispatcher("changePass.jsp").forward(request, response);
+            request.getRequestDispatcher("ChangePassword.jsp").forward(request, response);
 
         }
 
