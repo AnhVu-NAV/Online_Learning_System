de1d1b306be3ad22285716ec7490b43d589dbffc
Le Viet Nhat Huy <143341766+huylee4869@users.noreply.github.com>
2024-09-21 14:14:33 +0700

    Add Homepage

--- src/java/model/Blog.java
+++ src/java/model/Blog.java
@@ -4,7 +4,7 @@
  */
 package model;
 
-import java.sql.Timestamp;
+import java.util.Date;
 
 /**
  *
@@ -12,7 +12,6 @@ import java.sql.Timestamp;
  */
 public class Blog extends Blogs {
 
-    private int commentsCount;
     private String authorName;
 
     // Constructor không tham số
@@ -22,22 +21,12 @@ public class Blog extends Blogs {
 
     // Constructor có tham số
     public Blog(int blogId, int authorId, String title, String content, String thumbnailUrl,
-            String category, String briefInfo, Timestamp createdAt, Timestamp updatedAt,
-            int commentsCount, String authorName) {
+            String category, String briefInfo, Date createdAt, Date updatedAt, String authorName) {
         super(blogId, authorId, title, content, thumbnailUrl, category, briefInfo, createdAt, updatedAt);
-        this.commentsCount = commentsCount;
         this.authorName = authorName;
     }
 
     // Getters và setters
-    public int getCommentsCount() {
-        return commentsCount;
-    }
-
-    public void setCommentsCount(int commentsCount) {
-        this.commentsCount = commentsCount;
-    }
-
     public String getAuthorName() {
         return authorName;
     }
