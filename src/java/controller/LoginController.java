c3cab1041ad46966cacd68208c234d2803380d38
Le Viet Nhat Huy <143341766+huylee4869@users.noreply.github.com>
2024-09-21 14:14:33 +0700

    Add Homepage

--- src/java/controller/LoginController.java
+++ src/java/controller/LoginController.java
@@ -1,82 +1,58 @@
-///*
-// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
-// */
-//package controller;
-//
-////import dal.UserDAO;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
 //import dal.UserDAO;
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.http.HttpServlet;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import jakarta.servlet.http.HttpSession;
-//import java.io.IOException;
+import dal.UserDAO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import model.User;
 //import model.User;
-////import model.User;
-//
-///**
-// *
-// * @author AnhVuNAV
-// */
-//public class LoginController extends HttpServlet {
-//
-//    @Override
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        response.setContentType("text/html;charset=UTF-8");
-//        request.getRequestDispatcher("/Login.jsp").forward(request, response);
-//    }
-//
-//    @Override
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        response.setContentType("text/html;charset=UTF-8");
-//
-//        // Retrieve form data
-//        String email = request.getParameter("email");
-//        String password = request.getParameter("password");
-//        Integer status = null;
-//        try {
-//            status = Integer.parseInt(request.getParameter("status"));
-//        } catch (NumberFormatException e) {
-//            status = 1;
-//            request.setAttribute("error", "Invalid status.");
-//            request.getRequestDispatcher("/login.jsp").forward(request, response);
-//            return;
-//        }
-//
-//        HttpSession session = request.getSession();
-//        UserDAO userDao = new UserDAO();
-//        User user = userDao.getOne(email, password, status);
-//
-//        // Set user in session
-//        session.setAttribute("user", user);
-//
-//        if (user == null) {
-//            // Invalid email or password
-//            request.setAttribute("invalidUser", "Email or Password is invalid");
-//            request.getRequestDispatcher("/login.jsp").forward(request, response);
-//        } else if (user.getStatus() == 0) {
-//            // User is banned
-//            session.removeAttribute("user");
-//            response.sendRedirect("AccessDenied.jsp");
-//        } else {
-//            // Set fullname in session
-//            String fullname = user.getFirst_name() + " " + user.getLast_name();
-//            session.setAttribute("fullname", fullname);
-//
-//            if (user.getRole_id() == 0) {
-//                // Admin user
-//                response.sendRedirect(request.getContextPath() + "/admin");
-//            } else if (user.getRole_id() == 1) {
-//                // Customer user
-//                response.sendRedirect(request.getContextPath() + "/customer");
-//            }
-//
-//        }
-//    }
-//
-//   
-//}
-//
-//
-//
+
+/**
+ *
+ * @author AnhVuNAV
+ */
+public class LoginController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        request.getRequestDispatcher("/Login.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        HttpSession session = request.getSession();
+        UserDAO userDao = new UserDAO();
+        User user = userDao.getOne(username, password);
+        session.setAttribute("user", user);
+
+        if (session.getAttribute("user") == null) {
+            request.setAttribute("invalidUser", "Username or Password is invalid");
+            request.getRequestDispatcher("/Login.jsp").forward(request, response);
+        } else if (user.getBanned() == 1) {
+            response.sendRedirect("AccessDenied.jsp");
+            session.removeAttribute("user");
+        } else if (((User) session.getAttribute("user")).getRole_id() == 0) {
+            session.setAttribute("fullname", ((User) session.getAttribute("user")).getFullname());
+            request.getRequestDispatcher("admin").forward(request, response);
+        } else if (((User) session.getAttribute("user")).getRole_id() == 1) {
+            session.setAttribute("fullname", ((User) session.getAttribute("user")).getFullname());
+            response.sendRedirect("customer");
+        }
+    }
+
+}
