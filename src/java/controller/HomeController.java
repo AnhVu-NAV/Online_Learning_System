d30b2fd169d54fb5b282da5fae5c2acf29ea71b2
Le Viet Nhat Huy <143341766+huylee4869@users.noreply.github.com>
2024-09-23 05:10:12 +0700

    Add Authorization

--- src/java/controller/HomeController.java
+++ src/java/controller/HomeController.java
@@ -22,37 +22,29 @@ import model.User;
 @WebServlet(urlPatterns = {"/home", "/login", "/logout"})
 public class HomeController extends HttpServlet {
 
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-//        RequestDispatcher rd = request.getRequestDispatcher("/views/web/home.jsp");
+//        RequestDispatcher rd = request.getRequestDispatcher("/home.jsp");
         String action = request.getParameter("action");
+        HttpSession session = request.getSession(false);
+        User user = (User) (session != null ? session.getAttribute("user") : null);
+
         if (action != null && action.equals("login")) {
             String message = request.getParameter("message");
             String alert = request.getParameter("alert");
+
             if (message != null && alert != null) {
                 request.setAttribute("message", message);
                 request.setAttribute("alert", alert);
-
             }
 
-            RequestDispatcher rd = request.getRequestDispatcher("/login.jsp");
-            rd.forward(request, response);
-        } else if (action != null && action.equals("logout")) {
-            HttpSession session = request.getSession();
-            session.invalidate(); // Invalidate the current session
-            response.sendRedirect(request.getContextPath() + "/home"); // Redirect to login page
-        } else {
-            // Check if user is already logged in
-            HttpSession session = request.getSession(false);
-            User user = (User) session.getAttribute("user");
-//            RequestDispatcher rd = request.getRequestDispatcher("/home.jsp");
-//            rd.forward(request, response);
             if (user == null) {
-                // If no user is logged in, redirect to login
-                response.sendRedirect(request.getContextPath() + "/login?action=login");
+                // If no user is logged in, display home
+                request.getRequestDispatcher("/login.jsp").forward(request, response);
             } else {
                 // If user is logged in, handle role-based forwarding
-                if (user.getRole_id() == 0) {
+                if (user.getRole_id() == 6) {
                     // Admin user
                     response.sendRedirect(request.getContextPath() + "/admin");
                 } else if (user.getRole_id() == 1) {
@@ -60,49 +52,71 @@ public class HomeController extends HttpServlet {
                     response.sendRedirect(request.getContextPath() + "/customer");
                 }
             }
+        } else if (action != null && action.equals("logout")) {
+            if (session != null) {
+                session.invalidate(); // Invalidate the current session
+            }
+            response.sendRedirect(request.getContextPath() + "/home"); // Redirect to login page
+        } else {
+            // Check if user is already logged in
+//            HttpSession session = request.getSession(false);
+//            User user = (User) session.getAttribute("user");
+            if (user != null) {
+                String fullname = user.getFirst_name() + " " + user.getLast_name();
+                session.setAttribute("fullname", fullname);
+            }
+            request.getRequestDispatcher("/home.jsp").forward(request, response);
         }
     }
 
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String action = request.getParameter("action");
         if (action != null && action.equals("login")) {
-//            RequestDispatcher rd = request.getRequestDispatcher("/login.jsp");
-//            rd.forward(request, response);
             // Handle login via LoginController logic
-            String username = request.getParameter("username");
+            String email = request.getParameter("email");
             String password = request.getParameter("password");
 
             UserDAO userDao = new UserDAO();
-            User user = userDao.getOne(username, password, 0); // Assume not banned for login
+            User user = userDao.getOne(email, password, 1); // Assume not banned for login
 
             HttpSession session = request.getSession();
             session.setAttribute("user", user);
 
             if (user == null) {
                 // Invalid credentials, return to login page
-                request.setAttribute("invalidUser", "Username or Password is invalid");
+                request.setAttribute("message", "Email or password is invalid");
+                request.setAttribute("alert", "danger");
                 request.getRequestDispatcher("/login.jsp").forward(request, response);
                 // Add redirect after forwarding to login page
-                response.sendRedirect(request.getContextPath() + "/login?action=login&message='Username or password is invalid'&alert=danger");
-            } else if (user.getBanned() == 1) {
+//                response.sendRedirect(request.getContextPath() + "/login?action=login&message='Username or password is invalid'&alert=danger");
+            } else if (user.getStatus() == 0) {
                 // Banned user, redirect to access denied page
-                response.sendRedirect("AccessDenied.jsp");
                 session.removeAttribute("user");
+//                response.sendRedirect("AccessDenied.jsp");
+                request.setAttribute("message", "Account has been banned");
+                request.setAttribute("alert", "danger");
+                request.getRequestDispatcher("/login.jsp").forward(request, response);
             } else {
                 // Login successful, handle role-based forwarding
-                session.setAttribute("fullname", user.getFullname());
-
-                if (user.getRole_id() == 0) {
+                String fullname = (String) session.getAttribute("fullname");
+                if (user.getRole_id() == 6) {
                     // Admin user
-                    response.sendRedirect(request.getContextPath() + "/dashboard");
-                } else if (user.getRole_id() == 1) {
+//                    response.sendRedirect(request.getContextPath() + "/dashboard");
+                    response.sendRedirect(request.getContextPath() + "/admin/dashboard");
+
+                } else if (user.getRole_id() == 2) {
                     // Customer user
                     response.sendRedirect(request.getContextPath() + "/home");
-                } else if (user.getRole_id() == 2) {
+                } else if (user.getRole_id() == 0) {
                     // Default user
                     response.sendRedirect(request.getContextPath() + "/change-password");
+                } else {
+                    session.removeAttribute("user");
+                    response.sendRedirect(request.getContextPath() + "/home");
                 }
             }
         }
     }
+
 }
