6bb04be25d4788ad86f1e6769b1b4ec78290ddeb
Le Viet Nhat Huy <143341766+huylee4869@users.noreply.github.com>
2024-09-23 05:10:12 +0700

    Add Authorization

---
+++ src/java/controller/DashboardServlet.java
@@ -0,0 +1,32 @@
+package controller;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+import dal.BlogDAO;
+import jakarta.servlet.RequestDispatcher;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
+import model.Blog;
+
+/**
+ *
+ * @author mocun
+ */
+@WebServlet(urlPatterns = {"/admin/dashboard"})
+public class DashboardServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/admin/dashboard.jsp");
+        dispatcher.forward(request, response);
+    }
+}
