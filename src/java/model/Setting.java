10cd9d49d26582dbd02bedfb1207fd12baabe19f
AnhVuNAV <vunguyen.nav2004@gmail.com>
2024-09-23 10:49:16 +0700

    Add Course List Screen

--- src/java/model/Setting.java
+++ src/java/model/Setting.java
@@ -6,8 +6,53 @@ package model;
 
 /**
  *
- * @author mocun
+ * @author AnhVuNAV
  */
-class Setting {
-    
+public class Setting {
+    private int id;
+    private int settingTypeId;
+    private String value;
+    private int status;
+    private String description;
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSettingTypeId() {
+        return settingTypeId;
+    }
+
+    public void setSettingTypeId(int settingTypeId) {
+        this.settingTypeId = settingTypeId;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
