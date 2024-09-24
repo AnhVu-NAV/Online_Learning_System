7230efb94f06450d22e4c550408712db5bb597d7
Tran Xuan Hoan (K18 HL) <hoantxhe180030@fpt.edu.vn>
2024-09-24 21:54:04 +0700

    Reconfig the project

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -3,30 +3,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dal;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 /**
  *
  * @author AnhVuNAV
  */
 public class DBContext {
-
     protected Connection connection;
-
     public DBContext() {
         try {
             String user = "root"; // change this to your MySQL username
-            String pass = "hoan2709"; // change this to your MySQL password
-            String url = "jdbc:mysql://localhost:3306/Learnik"; // change mydb to your database name 
+            String pass = "123456"; // change this to your MySQL password
+            String url = "jdbc:mysql://localhost:3306/shopcard"; // change mydb to your database name
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(url, user, pass);
             System.out.println(connection);
-        } catch (ClassNotFoundException | SQLException ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
             Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -34,8 +29,5 @@ public class DBContext {
 
     public static void main(String[] args) {
         new DBContext(); // Just to test the connection
-//        if (connection == null) {
-//            System.out.println("null"); 
-//        }
     }
 }
