8f7432a1641a7819ae13f834368a1fe2afbae422
AnhVuNAV <vunguyen.nav2004@gmail.com>
2024-10-03 13:13:38 +0700

    update DAO for Course Detail

--- src/java/dal/UserDAO.java
+++ src/java/dal/UserDAO.java
@@ -34,8 +34,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
 import java.util.Vector;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.sql.Connection;
 import model.User;
 
 /**
