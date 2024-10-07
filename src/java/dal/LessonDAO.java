b33ac1eca5b9b2c6cd03bcfe8ac687976c0274de
AnhVuNAV <vunguyen.nav2004@gmail.com>
2024-10-07 16:09:00 +0700

    update DAO for course detail

--- src/java/dal/LessonDAO.java
+++ src/java/dal/LessonDAO.java
@@ -17,6 +17,7 @@ import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
 import dal.DBContext;
+import java.sql.SQLException;
 import model.Lesson;
 
 /**
@@ -94,6 +95,28 @@ public class LessonDAO extends DBContext {
                 lesson.setId(rs.getInt("id"));
                 lesson.setTitle(rs.getString("title"));
                 lesson.setStatus(rs.getInt("status"));
+                lesson.setLessonTypeId(rs.getInt("lesson_type_id"));
+                lessons.add(lesson);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return lessons;
+    }
+
+    // Lấy danh sách các bài học theo ID chương
+    public List<Lesson> getLessonsByChapterId(int chapterId){
+        List<Lesson> lessons = new ArrayList<>();
+        String sql = "SELECT * FROM Lesson WHERE chapter_id = ? AND status = 1 ORDER BY `order` ASC";
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, chapterId);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Lesson lesson = new Lesson();
+                lesson.setId(rs.getInt("id"));
+                lesson.setTitle(rs.getString("title"));
+                lesson.setOrder(rs.getInt("order"));
                 lesson.setLessonTypeId(rs.getInt("lesson_type_id"));
                 lessons.add(lesson);
             }
