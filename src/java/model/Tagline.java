668962774ecd0dfec16626f2c2af752129d55241
AnhVuNAV <vunguyen.nav2004@gmail.com>
2024-10-07 13:34:43 +0700

    add tagline model

---
+++ src/java/model/Tagline.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author AnhVuNAV
+ */
+public class Tagline {
+
+    private int id;
+    private String name;
+
+    public Tagline() {
+    }
+
+    public Tagline(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
