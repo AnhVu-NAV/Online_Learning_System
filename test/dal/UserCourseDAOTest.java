2715fe0abbb0e58242f2ec5274066fd3ca6c3ffa
Nguyen Anh Vu <vunguyen.nav2004@gmail.com>
2024-10-30 23:17:17 +0700

    add test

---
+++ test/dal/UserCourseDAOTest.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package dal;
+
+import model.UserCourse;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author AnhVuNAV
+ */
+public class UserCourseDAOTest {
+    
+    public UserCourseDAOTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of registerUserForCourse method, of class UserCourseDAO.
+     */
+    @Test
+    public void testRegisterUserForCourse() {
+        System.out.println("registerUserForCourse");
+        UserCourse userCourse = null;
+        UserCourseDAO instance = new UserCourseDAO();
+        instance.registerUserForCourse(userCourse);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
