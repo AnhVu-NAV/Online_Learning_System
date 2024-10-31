f1ff9768de70982eb0d940bab269ce29b49c8c97
VuLH <vulhhe130330@fpt.edu.vn>
2024-10-31 09:06:27 +0700

    Fix CourseDAO

--- src/java/dal/CourseDAO.java
+++ src/java/dal/CourseDAO.java
@@ -1,42 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dal;
 
 import model.Course;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Date;
 import java.util.Vector;
 import util.DataConvert;
 
-/**
- *
- * @author 84941
- */
 public class CourseDAO extends DBContext {
 
     DataConvert dc = new DataConvert();
 
+    // Insert a new course
     public int insertCourse(Course obj) {
         int n = 0;
-        String sql = "INSERT INTO Course\n"
-                + "           (title\n"
-                + "           ,subtitle\n"
-                + "           ,expert_id\n"
-                + "           ,total_duration\n"
-                + "           ,category_id\n"
-                + "           ,description\n"
-                + "           ,thumbnail_url\n"
-                + "           ,status\n"
-                + "           ,updated_date\n"
-                + "           ,created_date\n"
-                + "           ,number_of_learner)\n"
-                + "     VALUES\n"
-                + "           (?,?,?,?,?,?,?,?,?,?,?)";
+        String sql = "INSERT INTO Course "
+                + "(title, subtitle, expert_id, total_duration, category_id, description, status, updated_date, created_date, number_of_learner) "
+                + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         try {
             PreparedStatement pre = connection.prepareStatement(sql);
             pre.setString(1, obj.getTitle());
@@ -45,83 +26,43 @@ public class CourseDAO extends DBContext {
             pre.setInt(4, obj.getTotalDuration());
             pre.setInt(5, obj.getCategoryId());
             pre.setString(6, obj.getDescription());
-            pre.setString(7, obj.getThumbnailUrl());
-            pre.setInt(8, obj.getStatus());
-            pre.setDate(9, dc.UtilDateToSqlDate(obj.getUpdatedDate()));
-            pre.setDate(10, dc.UtilDateToSqlDate(obj.getCreatedDate()));
-            pre.setInt(11, obj.getNumberOfLearner());
-            
+            pre.setInt(7, obj.getStatus());
+            pre.setDate(8, dc.UtilDateToSqlDate(obj.getUpdatedDate()));
+            pre.setDate(9, dc.UtilDateToSqlDate(obj.getCreatedDate()));
+            pre.setInt(10, obj.getNumberOfLearner());
+
             n = pre.executeUpdate();
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
         return n;
-
     }
-    
-    
+
+    // Get courses based on custom SQL query
     public Vector<Course> getCourses(String sql) {
-        Vector<Course> vector = new Vector<Course>();
+        Vector<Course> vector = new Vector<>();
         try {
-            Statement state = connection.createStatement(
-                    ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
-            ResultSet rs = state.executeQuery(sql);
+            PreparedStatement state = connection.prepareStatement(sql);
+            ResultSet rs = state.executeQuery();
             while (rs.next()) {
-                int id = rs.getInt(1);
-                String title = rs.getString(2);
-                String subtitle = rs.getString(3);
-                int expertId = rs.getInt(4);
-                int totalDuration = rs.getInt(5);
-                int categoryId = rs.getInt(6);
-                String description = rs.getString(7);
-                String thumbnailUrl=rs.getString(8);
-                int status = rs.getInt(9);               
-                Date updatedDate=rs.getDate(10);
-                Date createdDate = rs.getDate(11);
-                int numberOfLearner=rs.getInt(12);
-                Course obj = new Course(id, title, subtitle, expertId, totalDuration, categoryId, description, thumbnailUrl, status, updatedDate, createdDate, numberOfLearner);
+                Course obj = mapResultSetToCourse(rs);
                 vector.add(obj);
-
             }
-
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
         return vector;
-
     }
+
+    // Get all courses
     public Vector<Course> getAll() {
         Vector<Course> vector = new Vector<>();
-        String sql = "SELECT id\n"
-                + "      ,title\n"
-                + "      ,subtitle\n"
-                + "      ,expert\n"
-                + "      ,total_duration\n"
-                + "      ,category_id\n"
-                + "      ,description\n"
-                + "      ,thumbnail_url\n"
-                + "      ,status\n"
-                + "      ,updated_date\n"
-                + "      ,created_date\n"
-                + "      ,number_of_learner\n"
-                + "  FROM course";
+        String sql = "SELECT id, title, subtitle, expert_id, total_duration, category_id, description, status, updated_date, created_date, number_of_learner FROM course";
         try {
-            Statement state = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
-            ResultSet rs = state.executeQuery(sql);
+            PreparedStatement state = connection.prepareStatement(sql);
+            ResultSet rs = state.executeQuery();
             while (rs.next()) {
-                int id = rs.getInt(1);
-                String title = rs.getString(2);
-                String subtitle = rs.getString(3);
-                int expertId = rs.getInt(4);
-                int totalDuration = rs.getInt(5);
-                int categoryId = rs.getInt(6);
-                String description = rs.getString(7);
-                String thumbnailUrl=rs.getString(8);
-                int status = rs.getInt(9);               
-                Date updatedDate=rs.getDate(10);
-                Date createdDate = rs.getDate(11);
-                int numberOfLearner=rs.getInt(12);
-                Course obj = new Course(id, title, subtitle, expertId, totalDuration, categoryId, description, thumbnailUrl, status, updatedDate, createdDate, numberOfLearner);
+                Course obj = mapResultSetToCourse(rs);
                 vector.add(obj);
             }
         } catch (SQLException ex) {
@@ -129,46 +70,37 @@ public class CourseDAO extends DBContext {
         }
         return vector;
     }
-    public Course getCourseById(int search_id) {
-        String sql = "SELECT id\n"
-                + "      ,title\n"
-                + "      ,subtitle\n"
-                + "      ,expert\n"
-                + "      ,total_duration\n"
-                + "      ,category_id\n"
-                + "      ,description\n"
-                + "      ,thumbnail_url\n"
-                + "      ,status\n"
-                + "      ,updated_date\n"
-                + "      ,created_date\n"
-                + "      ,number_of_learner\n"
-                + "  FROM course"
-                + "  WHERE Course.id = " + search_id;
 
+    // Get course by ID
+    public Course getCourseById(int searchId) {
+        String sql = "SELECT id, title, subtitle, expert_id, total_duration, category_id, description, status, updated_date, created_date, number_of_learner FROM course WHERE id = ?";
         try {
-            Statement state = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
-            ResultSet rs = state.executeQuery(sql);
-            while (rs.next()) {
-                int id = rs.getInt(1);
-                String title = rs.getString(2);
-                String subtitle = rs.getString(3);
-                int expertId = rs.getInt(4);
-                int totalDuration = rs.getInt(5);
-                int categoryId = rs.getInt(6);
-                String description = rs.getString(7);
-                String thumbnailUrl=rs.getString(8);
-                int status = rs.getInt(9);               
-                Date updatedDate=rs.getDate(10);
-                Date createdDate = rs.getDate(11);
-                int numberOfLearner=rs.getInt(12);
-                Course obj = new Course(id, title, subtitle, expertId, totalDuration, categoryId, description, thumbnailUrl, status, updatedDate, createdDate, numberOfLearner);
-                return obj;
+            PreparedStatement state = connection.prepareStatement(sql);
+            state.setInt(1, searchId);
+            ResultSet rs = state.executeQuery();
+            if (rs.next()) {
+                return mapResultSetToCourse(rs);
             }
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
         return null;
-
     }
 
+    // Helper method to map ResultSet to Course object
+    private Course mapResultSetToCourse(ResultSet rs) throws SQLException {
+        int id = rs.getInt("id");
+        String title = rs.getString("title");
+        String subtitle = rs.getString("subtitle");
+        int expertId = rs.getInt("expert_id");
+        int totalDuration = rs.getInt("total_duration");
+        int categoryId = rs.getInt("category_id");
+        String description = rs.getString("description");
+        int status = rs.getInt("status");
+        Date updatedDate = rs.getDate("updated_date");
+        Date createdDate = rs.getDate("created_date");
+        int numberOfLearner = rs.getInt("number_of_learner");
+
+        return new Course(id, title, subtitle, expertId, totalDuration, categoryId, description, status, updatedDate, createdDate, numberOfLearner);
+    }
 }
